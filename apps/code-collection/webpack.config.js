cbae66e5ccc1b2802ce708cfac4ec656d5c99096
April Gray <aprgray5@gmail.com>
2024-03-15 23:24:29 -0400

    Add tailwind

--- apps/code-collection/webpack.config.js
+++ apps/code-collection/webpack.config.js
@@ -27,4 +27,26 @@ module.exports = {
       // svgr: false
     }),
   ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'style-loader',
+          },
+          {
+            loader: 'css-loader',
+            options: {
+              importLoaders: 1,
+            }
+          },
+          {
+            loader: 'postcss-loader'
+          }
+        ]
+      }
+    ]
+  }
 };
--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,8 @@
 module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
+  plugins: [
+    require('autoprefixer'),
+    require('postcss-import'),
+    require('tailwindcss'),
+    // other PostCSS plugins
+  ]
 }
